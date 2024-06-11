d8580eeceb5e9a82d3d1e97613d2468d179476dc
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2024-06-11 06:09:26 +0530

    Time: 4 ms (100%), Space: 43.7 MB (68.23%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0)return false;
+        if(x<10)return true;
+        int n=x;
+        int rev=0;
+        while(n>0)
+        {
+            int rem =n%10;
+            rev=rev*10+rem;
+            n/=10;
+        }
+        return (rev==x);
+    }
+}
