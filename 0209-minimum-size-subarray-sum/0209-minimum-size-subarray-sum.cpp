4d06a3ff06af7fd458777e597f783592524b0c8e
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2024-06-12 06:41:10 +0530

    Time: 31 ms (28.85%), Space: 30.7 MB (17.39%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n =nums.size();
+       int r = 0,l=0;
+       int sum =0;
+       int m = INT_MAX;
+       while(r<n||l<n)
+       {
+            if(sum<target&&r<n)
+            {
+                sum+=nums[r];
+                r++;
+            }
+            else if(sum>=target)
+            {
+                m = min(m,r-l);
+                sum-=nums[l];
+                l++;
+            }
+            else
+            {
+                break;
+            }
+       }
+       return (m==INT_MAX)?0:m;
+    }
+};
