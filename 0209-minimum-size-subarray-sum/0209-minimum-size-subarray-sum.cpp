56b05bf19ee9c3b296d0961c8377277576963003
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2024-06-13 15:08:40 +0530

    Time: 18 ms (95.45%), Space: 30.4 MB (90.81%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -5,23 +5,23 @@ public:
        int r = 0,l=0;
        int sum =0;
        int m = INT_MAX;
-       while(r<n||l<n)
+       while(r<=n||l<=n)
        {
-            if(sum<target&&r<n)
-            {
-                sum+=nums[r];
-                r++;
-            }
-            else if(sum>=target)
-            {
-                m = min(m,r-l);
-                sum-=nums[l];
-                l++;
-            }
-            else
-            {
-                break;
-            }
+        if(sum<target&&r<n)
+        {
+            sum+=nums[r];
+            r++;
+        }
+        else if(sum>=target&&l<n)
+        {
+            m= min(m,r-l);
+            sum-=nums[l];
+            l++;
+        }
+        else
+        {
+            break;
+        }
        }
        return (m==INT_MAX)?0:m;
     }
