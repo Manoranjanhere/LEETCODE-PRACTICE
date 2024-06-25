c173ac7394029e3c27075aaf97e0bad6cc0b91eb
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2024-06-25 15:51:49 +0530

    Time: 143 ms (93.62%), Space: 116.7 MB (65.27%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -12,21 +12,17 @@ class Solution {
 public:
     bool isPalindrome(ListNode* head) {
         if(head==NULL)return NULL;
-        if(head->next==NULL)return head;
-        ListNode* temp = head;
-        ListNode* l3 = new ListNode();
-        ListNode* temp1 = l3;
-
-        while(temp!=NULL)
+        if(head->next==NULL)return true;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL&&fast->next!=NULL)
         {
-            ListNode* a=new ListNode(temp->val);
-            l3->next=a;
-            l3=l3->next;
-            temp=temp->next;
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        ListNode* prev = NULL;
-        ListNode* curr = head;
-        ListNode* next = head->next;
+        ListNode *prev=NULL;
+        ListNode *curr=slow;
+        ListNode *next=curr->next;
         while(next!=NULL)
         {
             curr->next=prev;
@@ -35,18 +31,15 @@ public:
             next=next->next;
         }
         curr->next=prev;
-        // return temp1->next;
-        temp1=temp1->next;
         while(curr!=NULL)
         {
-            if(temp1->val!=curr->val)
+            if(curr->val!=head->val)
             {
                 return false;
             }
             curr=curr->next;
-            temp1=temp1->next;
+            head=head->next;
         }
         return true;
-
     }
 };
