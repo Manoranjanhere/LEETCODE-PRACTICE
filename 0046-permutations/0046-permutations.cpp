548c53e44ab08a776f7511bb16878c11f26ad94a
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2024-06-29 00:18:35 +0530

    Time: 0 ms (100%), Space: 8.8 MB (88.9%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> res;
-        backtrack(nums, 0, res);
-        return res;
-    }
-
-    void backtrack(vector<int>& nums, int start, vector<vector<int>>& res) {
-        if (start == nums.size()) {
-            res.push_back(nums);
+        vector<vector<int>> ans;
+        vector<int> diff;
+        int n = nums.size();
+        vector<int> arr(n,false);
+        backtrack(nums,ans,diff,arr);
+        return ans;
+    }  
+    void backtrack(vector<int>& nums,vector<vector<int>>& ans,vector<int>& diff,vector<int>& arr)
+    {
+        if(diff.size()==nums.size())
+        {
+            ans.push_back(diff);
             return;
         }
-
-        for (int i = start; i < nums.size(); i++) {
-            swap(nums[start], nums[i]);
-            backtrack(nums, start + 1, res);
-            swap(nums[start], nums[i]);
+        for(int i=0;i<nums.size();i++)
+        {
+            if(!arr[i])
+            {
+                diff.push_back(nums[i]);
+                arr[i]=true;
+                backtrack(nums,ans,diff,arr);
+                arr[i]=false;
+                diff.pop_back();
+            }
         }
     }
     
-    void swap(int& a, int& b) {
-        int temp = a;
-        a = b;
-        b = temp;
-    }    
+
 };
