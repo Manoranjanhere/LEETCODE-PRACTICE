92aeb2deff6603e66a7ecc0b879a06d8b6ee53f8
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2024-07-12 14:44:55 +0530

    Time: 0 ms (100%), Space: 8.1 MB (17.58%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return solve(0,0,m-1,n-1,dp);
-    }
-    int solve(int r,int d,int m,int n,vector<vector<int>>& dp)
-    {
-        if(r>m||d>n)
-        {
-            return 0;
+        // Create a 2D vector dp with m rows and n columns initialized to 1
+        vector<vector<int>> dp(m, vector<int>(n, 1));
+        
+        // Iterate over the grid starting from cell (1,1)
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                // The number of ways to reach cell (i,j) is the sum of ways to reach (i-1,j) and (i,j-1)
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
         }
-
-        if(r==m&&d==n)
-        {
-            return 1;
-        }
-        if(dp[r][d]!=-1)
-        {
-            return dp[r][d];
-        }
-        dp[r][d]=solve(r+1,d,m,n,dp)+solve(r,d+1,m,n,dp);
-        return dp[r][d];
+        
+        // The bottom-right cell contains the number of unique paths
+        return dp[m-1][n-1];
     }
 };
