3d8429b2e41665b7c377fec2ef37abb3a0e52e90
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2024-09-11 23:20:08 +0530

    Time: 7 ms (83.38%), Space: 13.8 MB (46.17%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,42 +1,30 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        int n = s.size();
-        string ans="";
-        int w=0;
-        string arr[numRows];
-        while(w!=n)
+        vector<string> ans(numRows,""); 
+        int j=0,dir=-1;
+        if(numRows<=1)return s;
+        for(int i=0;i<s.length();i++)
         {
-            for(int i=0;i<numRows;i++)
+            if(j==numRows-1||j==0)
             {
-                if(w<n)
-                {
-                    arr[i]+=s[w];
-                    w++;
-                }
-                else
-                {
-                    break;
-                }
+                dir*=-1;
             }
-            for(int i=numRows-2;i>0;i--)
+            ans[j]+=s[i];
+            if(dir==1)
             {
-                if(w<n)
-                {
-                    arr[i]+=s[w];
-                    w++;
-                }
-                else
-                {
-                    break;
-                }
+                j++;
             }
-            
+            else
+            {
+                j--;
+            }
         }
-        for(int i=0;i<numRows;i++)
+        string res;
+        for(auto it:ans)
         {
-            ans+=arr[i];
+            res+=it;
         }
-        return ans;
+        return res;
     }
 };
