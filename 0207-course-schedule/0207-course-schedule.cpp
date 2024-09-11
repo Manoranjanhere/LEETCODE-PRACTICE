01ba832658b32a0a91733549823df53da0ac5b53
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2024-09-11 15:39:27 +0530

    Time: 23 ms (14.63%), Space: 19.3 MB (13.08%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+bool dfs(int node,map<int,vector<int>>&mp,vector<int>&vis,vector<int>& path)
+{
+    vis[node]=1;
+    path[node]=1;
+    for(auto neighbour:mp[node])
+    {
+        if(!vis[neighbour])
+        {
+            if(dfs(neighbour,mp,vis,path))
+            {
+                return true;
+            }
+        }
+        else if(path[neighbour])
+        {
+            return true;
+        }
+
+    }
+    path[node]=0;
+    return false;
+
+}
+    bool canFinish(int numCourses, vector<vector<int>>& p) {
+        map<int,vector<int>>mp;
+        vector<int> vis(numCourses,0);
+        vector<int> path(numCourses,0);
+        int n = p.size();
+        for(int i=0;i<n;i++)
+        {
+            mp[p[i][0]].push_back(p[i][1]);
+        }
+        for(int i=0;i<numCourses;i++)
+        {
+            if(!vis[i])
+            {
+                if(dfs(i,mp,vis,path))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
