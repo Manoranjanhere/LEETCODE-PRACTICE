c8687a0916f1e1dece459952c94a8292a7ff0623
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2024-09-18 14:44:20 +0530

    Time: 4 ms (40.76%), Space: 9.1 MB (22.09%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.cpp
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.cpp
@@ -1,24 +1,51 @@
 class Solution {
 public:
-    map<string,int>mp;
-    
-    void stringstreeam(string s)
-    {
-        stringstream ss(s);
-        string word;
-        while(ss>>word) mp[word]++;
-    }
-
-    vector<string> uncommonFromSentences(string s1, string s2) 
-    {
-        stringstreeam(s1);
-        stringstreeam(s2);
-
-        vector<string>ans;
-        for(auto x:mp)
-            if(x.second==1) 
-                ans.push_back(x.first);
-
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        unordered_map<string,int> st1;
+        unordered_map<string,int> st2;
+        vector<string> ans;
+        string store="";
+        for(auto i:s1)
+        {
+            if(i!=' ')
+            {
+                store+=i;
+            }
+            else
+            {
+                st1[store]++;
+                store="";
+            }
+        }
+        st1[store]++;
+        store="";
+        for(auto i:s2)
+        {
+            if(i!=' ')
+            {
+                store+=i;
+            }
+            else
+            {
+                st2[store]++;
+                store="";
+            }
+        }
+        st2[store]++;
+        for(auto i:st1)
+        {
+            if(st2.find(i.first)==st2.end()&&st1[i.first]==1)
+            {
+                ans.push_back(i.first);
+            }
+        }
+        for(auto i:st2)
+        {
+            if(st1.find(i.first)==st1.end()&&st2[i.first]==1)
+            {
+                ans.push_back(i.first);
+            }
+        }
         return ans;
     }
 };
