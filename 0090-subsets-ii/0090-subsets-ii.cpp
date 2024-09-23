fb41f55bf9430d29004ed2652b0d62ceb7dca7ff
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2024-09-23 13:55:37 +0530

    Time: 0 ms (100%), Space: 9 MB (79.86%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>>  ans;
+        vector<vector<int>> ans;
         vector<int> store;
-        int l=0;
+        int l = 0;
         int n = nums.size();
         sort(nums.begin(), nums.end()); // Sort to handle duplicates
-        solve(nums,ans,store,l,n);
-        sort(ans.begin(),ans.end());
+        solve(nums, ans, store, l, n);
         return ans;
     }
-    void solve(vector<int>& nums,vector<vector<int>>&ans,vector<int>& store,int l,int n)
-    {
-        if(l==n)
-        {
-            if (find(ans.begin(), ans.end(), store) == ans.end())
-            {
-                ans.push_back(store);
-            }
-            return;
+
+    void solve(vector<int>& nums, vector<vector<int>>& ans, vector<int>& store, int l, int n) {
+        ans.push_back(store);
+
+        for (int i = l; i < n; i++) {
+            if (i > l && nums[i] == nums[i - 1]) continue;
+            
+            store.push_back(nums[i]);
+            solve(nums, ans, store, i + 1, n);
+
+            // Backtrack (remove the current element)
+            store.pop_back();
         }
-        solve(nums,ans,store,l+1,n);
-        store.push_back(nums[l]);
-        solve(nums,ans,store,l+1,n);
-        store.pop_back();
     }
 };
