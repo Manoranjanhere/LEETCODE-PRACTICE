748a9bd4a48ffc7fe4a8da9497a95b529e2b2a80
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2024-09-27 12:02:32 +0530

    Time: 745 ms (18.69%), Space: 10.9 MB (93.11%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int c=0;
-        solve(nums,target,c,0,0);
+        solve(nums,target,c,0);
         return c;
     }
-    void solve(vector<int>& nums,int target,int &c,int l,int cu)
+    void solve(vector<int>& nums,int target,int &c,int l)
     {
         if(l==nums.size())
         {
@@ -16,8 +16,8 @@ public:
             return;
         }
        
-            solve(nums,target-nums[l],c,l+1,cu);
-            solve(nums,target+nums[l],c,l+1,cu);
+            solve(nums,target-nums[l],c,l+1);
+            solve(nums,target+nums[l],c,l+1);
     }
 
 };
