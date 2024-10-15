85418d08d26566d3698e4f20815bef60a580038b
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2024-10-15 11:09:03 +0530

    Time: 21 ms (53.01%), Space: 22 MB (89.84%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -4,16 +4,23 @@ public:
     NumArray(vector<int>& nums) 
     {
         int n = nums.size();
-        ans.push_back(0);
-        for(int i=0;i<n;i++)
+        ans.push_back(nums[0]);
+        for(int i=1;i<n;i++)
         {
-            ans.push_back(ans[i]+nums[i]);
+            ans.push_back(ans[i-1]+nums[i]);
         }
     }
     
     int sumRange(int left, int right) 
     {
-        return ans[right+1]-ans[left];
+        if(left!=0)
+        {
+        return ans[right]-ans[left-1];
+        }
+        else
+        {
+            return ans[right];
+        }
     }
 };
 
