95de502dd9845e70873ea27d8bd738928b468956
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2024-10-17 01:33:53 +0530

    Time: 17 ms (49.16%), Space: 40.1 MB (21.79%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int c=0;
-        solve(nums,target,c,0);
-        return c;
+        // Memoization table: 2D array with size (nums.size() x 2001)
+        // We use 2001 to handle sums from -1000 to +1000 (offset by 1000)
+        vector<vector<int>> dp(nums.size(), vector<int>(2001, -1));
+        return solve(nums, target, 0, 0, dp);
     }
-    void solve(vector<int>& nums,int target,int &c,int l)
-    {
-        if(l==nums.size())
-        {
-            if(target==0)
-            {
-                c++;
-            }
-            return;
+
+    int solve(vector<int>& nums, int target, int sum, int index, vector<vector<int>>& dp) {
+        // Base case: reached the end of the nums array
+        if (index == nums.size()) {
+            // If the sum equals the target, count this as a valid way
+            return sum == target ? 1 : 0;
         }
-       
-            solve(nums,target-nums[l],c,l+1);
-            solve(nums,target+nums[l],c,l+1);
+
+        // If the result for this state is already computed, return it
+        if (dp[index][sum + 1000] != -1) {
+            return dp[index][sum + 1000];
+        }
+
+        // Recursively calculate the number of ways by adding or subtracting current number
+        int add = solve(nums, target, sum + nums[index], index + 1, dp);
+        int subtract = solve(nums, target, sum - nums[index], index + 1, dp);
+
+        // Store the result in the dp table
+        dp[index][sum + 1000] = add + subtract;
+
+        // Return the total number of ways for this state
+        return dp[index][sum + 1000];
     }
 };
