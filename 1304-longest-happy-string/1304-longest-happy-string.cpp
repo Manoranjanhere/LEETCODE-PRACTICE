3a0fceaa284783aed125d0885e20f806eaf26b37
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2024-10-17 15:53:57 +0530

    Time: 0 ms (100%), Space: 7.7 MB (63.68%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.cpp
+++ 1304-longest-happy-string/1304-longest-happy-string.cpp
@@ -5,44 +5,39 @@ using namespace std;
 class Solution {
 public:
     string longestDiverseString(int a, int b, int c) {
-        // Priority queue to store the characters and their counts.
-        priority_queue<pair<int, char>> pq;
-        if (a > 0) pq.push({a, 'a'});
-        if (b > 0) pq.push({b, 'b'});
-        if (c > 0) pq.push({c, 'c'});
-
-        string result = "";
-
-        while (!pq.empty()) {
-            // Get the character with the most count.
-            auto [count1, char1] = pq.top();
+        priority_queue<pair<int,char>> pq;
+        if(a>0)pq.push({a,'a'});
+        if(b>0)pq.push({b,'b'});
+        if(c>0)pq.push({c,'c'});
+        string ans="";
+        while(!pq.empty())
+        {
+            auto a=pq.top();
             pq.pop();
-
-            // Check if the last two characters in result are the same.
-            if (result.size() >= 2 && result.back() == char1 && result[result.size() - 2] == char1) {
-                if (pq.empty()) break;  // No valid characters left.
-
-                // Get the second most character.
-                auto [count2, char2] = pq.top();
+            if(ans.size()>=2&& ans[ans.size()-1]==a.second&&ans[ans.size()-2]==a.second)
+            {
+                if(pq.empty())
+                {
+                    break;
+                }
+                auto b = pq.top();
                 pq.pop();
-
-                // Add the second character to avoid consecutive repetition.
-                result += char2;
-                count2--;
-
-                if (count2 > 0) pq.push({count2, char2});
-
-                // Push back the most frequent character for later use.
-                pq.push({count1, char1});
-            } else {
-                // If no repetition issue, add the most frequent character.
-                result += char1;
-                count1--;
-
-                if (count1 > 0) pq.push({count1, char1});
+                ans+=b.second;
+                if(b.first-1>0)
+                {
+                    pq.push({b.first-1,b.second});
+                }
+                pq.push({a.first,a.second});
+            }
+            else
+            {
+                ans+=a.second;  
+                if(a.first-1>0)
+                {
+                    pq.push({a.first-1,a.second});
+                }
             }
         }
-
-        return result;
+        return ans;
     }
 };
