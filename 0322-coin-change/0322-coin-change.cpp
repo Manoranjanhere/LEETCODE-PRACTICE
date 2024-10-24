b0738cfeb0475d9eb11dbcfb602396a3fc38f64a
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2024-10-24 11:05:33 +0530

    Time: 19 ms (87.55%), Space: 16.5 MB (72.2%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -7,21 +7,18 @@ using namespace std;
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-            // Initialize the DP array with amount + 1 (which is an impossible value)
                     vector<int> dp(amount + 1, amount + 1);
-                            dp[0] = 0; // Base case: 0 coins needed to make amount 0
-
-                                    // Fill the DP table
-                                            for (int i = 1; i <= amount; i++) {
-                                                        for (const int& coin : coins) {
-                                                                        if (i - coin >= 0) { // Ensure we don't go negative
-                                                                                            dp[i] = min(dp[i], dp[i - coin] + 1); // Update dp[i]
-                                                                                                            }
-                                                                                                                        }
-                                                                                                                                }
-
-                                                                                                                                        // Return the result
-                                                                                                                                                return (dp[amount] == amount + 1) ? -1 : dp[amount];
-                                                                                                                                                    }
-                                                                                                                                                    };
+                    dp[0] = 0;
+                    for (int i = 1; i <= amount; i++) 
+                    {
+                        for (const int& coin : coins) 
+                        {
+                            if (i - coin >= 0) 
+                            { 
+                                dp[i] = min(dp[i], dp[i - coin] + 1);
+                            }
+                        }
+                    }                                                                                                                     return (dp[amount] == amount + 1) ? -1 : dp[amount];
+                    }
+                    };
                                                                                                                                                     
