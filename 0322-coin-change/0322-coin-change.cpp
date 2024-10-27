dc2f5ebd76e337c1f6c737ae17b93493b5292838
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2024-10-28 03:07:06 +0530

    Time: 68 ms (13.48%), Space: 52.3 MB (10.32%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,24 +1,20 @@
-#include <vector>
-#include <algorithm>
-#include <limits.h>
-
-using namespace std;
-
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-                    vector<int> dp(amount + 1, amount + 1);
-                    dp[0] = 0;
-                    for (int i = 1; i <= amount; i++) 
-                    {
-                        for (const int& coin : coins) 
-                        {
-                            if (i - coin >= 0) 
-                            { 
-                                dp[i] = min(dp[i], dp[i - coin] + 1);
-                            }
-                        }
-                    }                                                                                                                     return (dp[amount] == amount + 1) ? -1 : dp[amount];
-                    }
-                    };
-                                                                                                                                                    
+        vector<vector<int>>dp(coins.size()+1,vector<int>(amount+1,-1));
+        int n=solve(0,coins,amount,dp);
+        return n==INT_MAX?-1:n;
+    }
+    int solve(int i,vector<int>&coins,int amount,vector<vector<int>>&dp){
+        if(i>=coins.size()||amount<=0){
+       if(amount==0) return 0;
+       return INT_MAX;}
+        if(dp[i][amount]!=-1)return dp[i][amount];
+
+        int include=solve(i,coins,amount-coins[i],dp);
+        if(include!=INT_MAX)include++;
+        int exclude=solve(i+1,coins,amount,dp);
+        
+        return dp[i][amount]= min(include,exclude);
+    }
+};
