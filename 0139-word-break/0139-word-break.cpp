f0702d83f8d7e3089d255955b63f3c48aa2f5e7a
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2024-10-29 05:14:12 +0530

    Time: 4 ms (64.79%), Space: 14.4 MB (53.74%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,26 +1,36 @@
-#include <vector>
+#include <iostream>
 #include <string>
-#include <unordered_set>
+#include <vector>
+#include <unordered_map>
 
 class Solution {
 public:
     bool wordBreak(std::string s, std::vector<std::string>& wordDict) {
-            int len = s.length();
-                    std::vector<bool> dp(len + 1, false);
-                            dp[0] = true; // Base case: empty string can be formed
+        std::unordered_map<std::string, bool> dp;
+        return solve(s, wordDict, dp);
+    }
 
-                                    std::unordered_set<std::string> wordSet(wordDict.begin(), wordDict.end());
+private:
+    bool solve(std::string s, std::vector<std::string>& wordDict, std::unordered_map<std::string, bool>& dp) {
+        if (s.empty()) {
+            return true; // Base case: If s is empty, we've successfully segmented it
+        }
 
-                                            for (int i = 1; i <= len; i++) {
-                                                        for (int j = 0; j < i; j++) {
-                                                                        if (dp[j] && wordSet.find(s.substr(j, i - j)) != wordSet.end()) {
-                                                                                            dp[i] = true;
-                                                                                                                break; // Found a valid segmentation
-                                                                                                                                }
-                                                                                                                                            }
-                                                                                                                                                    }
+        if (dp.find(s) != dp.end()) {
+            return dp[s]; // Return memoized result if we already computed this substring
+        }
 
-                                                                                                                                                            return dp[len]; // Return whether the entire string can be formed
-                                                                                                                                                                }
-                                                                                                                                                                };
-                                                                                                                                                                
+        for (const std::string& word : wordDict) {
+            if (s.substr(0, word.size()) == word) { // Check if s starts with the word
+                std::string remainder = s.substr(word.size()); // Remaining substring after removing the word
+                if (solve(remainder, wordDict, dp)) {
+                    dp[s] = true; // Memoize and return true if we found a valid segmentation
+                    return true;
+                }
+            }
+        }
+
+        dp[s] = false; // Memoize and return false if no segmentation found
+        return false;
+    }
+};
