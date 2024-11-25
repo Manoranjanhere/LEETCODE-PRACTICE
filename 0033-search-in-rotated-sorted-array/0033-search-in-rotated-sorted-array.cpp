c8f3484bee4530605d06b3cb6f867add226cd310
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2024-11-25 14:35:27 +0530

    Time: 0 ms (100%), Space: 13.9 MB (82.2%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int l= 0;
-        int first =l;
-        int r= nums.size()-1;
-        int last =r;
-        while(l<=r)
+    int search(vector<int>& arr, int target) {
+        int n = arr.size();
+        int l=0;
+        int h=n-1;
+        while(l<=h)
         {
-            int mid = (l+r)/2;
-            if(nums[mid]==target)
+            int mid = (l+h)/2;
+            if(arr[mid]==target)
             {
                 return mid;
             }
-            else if(nums[mid]>nums[last])
+            if(arr[l]<=arr[mid])
             {
-                if(nums[mid]>target&&nums[last]<target)
+                if(target >= arr[l] && target < arr[mid])
                 {
-                    r=mid-1;
+                    h=mid-1;
                 }
                 else
                 {
                     l=mid+1;
                 }
             }
-            else if(nums[mid]<nums[last]&&nums[mid]<target&&nums[last]>=target)
+            else
             {
-                l=mid+1;
-            }
-            else 
-            {
-                r=mid-1;
+                if(target > arr[mid] && target <= arr[h])
+                {
+                    l=mid+1;
+                }
+                else
+                {
+                    h=mid-1;
+                }
             }
         }
         return -1;
