f91dc997c8e0c09c691258347a25ca99fd9b3162
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2024-12-09 14:18:34 +0530

    Time: 219 ms (7.03%), Space: 126.7 MB (63.34%) - LeetHub

--- 3427-special-array-ii/3427-special-array-ii.cpp
+++ 3427-special-array-ii/3427-special-array-ii.cpp
@@ -15,18 +15,9 @@ public:
             pref[i]=c;
             cout<<pref[i];
         }
-        int sec=0;
         for(int i=0;i<queries.size();i++)
         {
-            if(queries[i][0]==0)
-            {
-                sec=0;
-            }
-            else
-            {
-                sec=pref[queries[i][0]];
-            }
-            if((pref[queries[i][1]]-sec)!=0)
+            if((pref[queries[i][1]]-pref[queries[i][0]])!=0)
             {
                 ans.push_back(false);
             }
