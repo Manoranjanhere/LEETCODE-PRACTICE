4b5d693c7637e8aa0891bc116227519635f87c73
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2024-12-21 15:50:35 +0530

    Time: 0 ms (100%), Space: 24.3 MB (7.18%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        stable_partition(nums.begin(),nums.end(),[](int n)
+        {
+            return n!=0;
+        });
+    }
+};
