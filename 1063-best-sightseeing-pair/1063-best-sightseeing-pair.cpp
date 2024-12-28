f6fc4c67ee2b3c8f882b1c5da3ef2c46cbf3071f
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2024-12-29 03:05:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
+        vector<int> prefsum;
+        prefsum.push_back(values[0]);
+        int ans=0;
         int n = values.size();
-        if (n < 2) return values[0] + values[1] + 0 - 1; 
-        vector<int> suffixMax(n);
-        suffixMax[n - 1] = values[n - 1] - (n - 1);
-        for (int i = n - 2; i >= 0; i--) {
-            suffixMax[i] = max(suffixMax[i + 1], values[i] - i);
+        for(int i=1;i<n;i++)
+        {
+            if(prefsum[i-1]<values[i]+i)
+            {
+                prefsum.push_back(values[i]+i);
+            }
+            else
+            {
+                prefsum.push_back(values[i-1]+i-1);
+            }
         }
-        int maxScore = INT_MIN;
-        for (int i = 0; i < n - 1; i++) {
-            maxScore = max(maxScore, values[i] + i + suffixMax[i + 1]);
+        for(int i=1;i<n;i++)
+        {
+            ans=max(ans,prefsum[i-1]+values[i]-i);
         }
-
-        return maxScore;
+        return ans;
     }
 };
