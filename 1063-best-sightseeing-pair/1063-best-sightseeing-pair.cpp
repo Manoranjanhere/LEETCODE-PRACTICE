1034d090fcaf5ad2079376fa2c264ec8c9ef32d2
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2024-12-29 03:08:27 +0530

    Time: 0 ms (100%), Space: 45.7 MB (37.78%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
-        vector<int> prefsum;
-        prefsum.push_back(values[0]);
+        int maxstore= values[0];
         int ans=0;
         int n = values.size();
         for(int i=1;i<n;i++)
         {
-            if(prefsum[i-1]<values[i]+i)
-            {
-                prefsum.push_back(values[i]+i);
-            }
-            else
-            {
-                prefsum.push_back(prefsum[i-1]);
-            }
-        }
-        for(int i=1;i<n;i++)
-        {
-            ans=max(ans,prefsum[i-1]+values[i]-i);
+            ans=max(ans,maxstore+values[i]-i);
+            maxstore=max(maxstore,values[i]+i);
         }
         return ans;
     }
