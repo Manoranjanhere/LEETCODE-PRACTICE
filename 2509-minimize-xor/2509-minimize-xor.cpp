a45fb6de4f6c9448abced32e6e8e7f99c343144f
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2025-01-15 20:37:14 +0530

    Time: 1436 ms (2.34%), Space: 8 MB (40.19%) - LeetHub

--- 2509-minimize-xor/2509-minimize-xor.cpp
+++ 2509-minimize-xor/2509-minimize-xor.cpp
@@ -2,22 +2,47 @@ class Solution {
 public:
     int minimizeXor(int num1, int num2) {
         int n = __builtin_popcount(num2); 
-        int result = 0;
-
-        for (int i = 31; i >= 0 && n > 0; --i) {
-            if ((num1 >> i) & 1) { 
-                result |= (1 << i); 
-                --n;
+        int w=num1;
+        int s1;
+        int s2;
+        while(true)
+        {
+            if(__builtin_popcount(w)==n)
+            {
+                s1=w;
+                break;
+            }
+            w++;
+        }
+        w=num1-1;
+        bool workdone=false;
+        while(w>0)
+        {
+            if(__builtin_popcount(w)==n)
+            {
+                s2=w;
+                workdone=true;
+                break;
+            }
+            w--;
+        }
+        if(!workdone)
+        {
+            return s1;
+        }
+        else
+        {
+            int f = s1^num1;
+            int s = s2^num1;
+            if(f>s)
+            {
+                return s2;
+            }
+            else
+            {
+                return s1;
             }
         }
 
-        for (int i = 0; i <= 31 && n > 0; ++i) {
-            if (!((result >> i) & 1)) { 
-                result |= (1 << i); 
-                --n;
-            }
-        }
-
-        return result;
     }
 };
