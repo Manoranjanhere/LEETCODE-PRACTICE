d211df56414201d54fffb78534520c55013924b5
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2025-01-18 15:19:28 +0530

    Time: 54 ms (20.98%), Space: 49.3 MB (28.2%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>>dp(coins.size()+1,vector<int>(amount+1,-1));
-        int n=solve(0,coins,amount,dp);
-        return n==INT_MAX?-1:n;
+        int ind = coins.size();
+        vector<vector<int>>dp(ind,vector<int>(amount+1,-1));
+        int ans =solve(coins,ind-1,amount,dp);
+        if(ans==1e9)
+        {
+            return -1;
+        }
+        return ans;
     }
-    int solve(int i,vector<int>&coins,int amount,vector<vector<int>>&dp){
-        if(i>=coins.size()||amount<=0){
-       if(amount==0) return 0;
-       return INT_MAX;}
-        if(dp[i][amount]!=-1)return dp[i][amount];
+    int solve(vector<int>& coins,int ind, int amount,vector<vector<int>>&dp)
+    {
+        if(dp[ind][amount]!=-1)return dp[ind][amount];
+        if(ind==0)
+        {
+            if(amount%coins[ind]==0)
+            {
+                return amount/coins[ind];
+            }
+            return 1e9;
+        }
 
-        int include=solve(i,coins,amount-coins[i],dp);
-        if(include!=INT_MAX)include++;
-        int exclude=solve(i+1,coins,amount,dp);
-        
-        return dp[i][amount]= min(include,exclude);
+        int nottake=0+solve(coins,ind-1,amount,dp);
+        int take=1e9;
+        if(amount-coins[ind]>=0)
+        {
+            take=1+solve(coins,ind,amount-coins[ind],dp);
+        }
+        return dp[ind][amount]=min(nottake,take);
     }
 };
