607ee90465996c1142df77d93528989f9f364f79
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2025-01-28 12:45:35 +0530

    Time: 3 ms (84.06%), Space: 99.8 MB (30.47%) - LeetHub

--- 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
@@ -3,7 +3,7 @@ public:
     int findMaxFish(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        stack<vector<pair<int,int>>> st;
+        stack<pair<int,int>> st;
         vector<vector<bool>>store(n,vector<bool>(m,false));
         int ans = INT_MIN;
         for(int i=0;i<n;i++)
@@ -12,32 +12,32 @@ public:
             {
                 if(grid[i][j]!=0&&!store[i][j])
                 {
-                    st.push({{i,j}});
+                    st.push({i,j});
                     int sum=0;
                     while(!st.empty())
                     {
                         auto curr = st.top();
                         st.pop();
-                        int f = curr[0].first;
-                        int s = curr[0].second;
+                        int f = curr.first;
+                        int s = curr.second;
                         if(store[f][s])continue;
                         store[f][s]=true;
                         sum+=grid[f][s];
                         if(s<m-1&&grid[f][s+1]!=0&&!store[f][s+1])
                         {
-                            st.push({{f,s+1}});
+                            st.push({f,s+1});
                         }
                         if(s>0&&grid[f][s-1]!=0&&!store[f][s-1])
                         {
-                            st.push({{f,s-1}});
+                            st.push({f,s-1});
                         }
                         if(f<n-1&&grid[f+1][s]!=0&&!store[f+1][s])
                         {
-                            st.push({{f+1,s}});
+                            st.push({f+1,s});
                         }
                         if(f>0&&grid[f-1][s]!=0&&!store[f-1][s])
                         {
-                            st.push({{f-1,s}});
+                            st.push({f-1,s});
                         }
                     }
                     ans=max(ans,sum);
