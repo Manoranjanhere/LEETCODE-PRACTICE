17c8d30053b9e84472e70fa842b33c11b20fb898
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2025-01-28 14:54:59 +0530

    Time: 56 ms (18.13%), Space: 49.5 MB (24.67%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        int ind = coins.size();
-        vector<vector<int>>dp(ind,vector<int>(amount+1,-1));
-        int ans =solve(coins,ind-1,amount,dp);
-        if(ans==1e9)
-        {
-            return -1;
-        }
-        return ans;
+        int n = coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+       int ans = solve(coins,amount,n-1,dp);
+       return (ans==1e9)?-1:ans;
     }
-    int solve(vector<int>& coins,int ind, int amount,vector<vector<int>>&dp)
+    int solve(vector<int>& coins, int amount,int ind,vector<vector<int>>&dp)
     {
-        if(dp[ind][amount]!=-1)return dp[ind][amount];
+        if(dp[ind][amount]!=-1) return dp[ind][amount];
         if(ind==0)
         {
             if(amount%coins[ind]==0)
             {
                 return amount/coins[ind];
             }
-            return 1e9;
+            else
+            {
+                return 1e9;
+            }
         }
-
-        int nottake=0+solve(coins,ind-1,amount,dp);
-        int take=1e9;
-        if(amount-coins[ind]>=0)
+        int nottake= solve(coins,amount,ind-1,dp);
+        int take = 1e9;
+        if(amount>=coins[ind])
         {
-            take=1+solve(coins,ind,amount-coins[ind],dp);
+            take =1+solve(coins,amount-coins[ind],ind,dp);
         }
-        return dp[ind][amount]=min(nottake,take);
+        return dp[ind][amount] = min(take,nottake);
     }
+
 };
