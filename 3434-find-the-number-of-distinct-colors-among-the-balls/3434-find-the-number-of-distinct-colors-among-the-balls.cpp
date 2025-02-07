2444cd0e0fd3a905bdfcf04e1ede126119e851d0
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2025-02-07 13:12:57 +0530

    Time: 1733 ms (5.15%), Space: 157.6 MB (56.59%) - LeetHub

---
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    vector<int> queryResults(int limit, vector<vector<int>>& queries) {
+        unordered_map<int,int> un1;
+        unordered_map<int,int> mp;
+        vector<int> ans;
+        int c=0;
+        for(int i=0;i<queries.size();i++)
+        {
+            if(mp.find(queries[i][0])==mp.end()&&un1.find(queries[i][1])==un1.end())
+            {
+                c++;
+                mp[queries[i][0]]=queries[i][1];
+                un1[queries[i][1]]++;
+                cout<<"1 "<<queries[i][0]<<" "<<un1[mp[queries[i][0]]]<<" ";
+                
+            }
+            else if(mp.find(queries[i][0])!=mp.end()&&un1.find(queries[i][1])==un1.end())
+            {
+                un1[mp[queries[i][0]]]--;
+                if(un1[mp[queries[i][0]]]==0)
+                {
+                    un1.erase(mp[queries[i][0]]);
+                }
+                else
+                {
+                    c++;
+                }
+                mp[queries[i][0]]=queries[i][1];
+                un1[queries[i][1]]++;
+                cout<<"2 "<<queries[i][0]<<" "<<un1[mp[queries[i][0]]]<<" ";
+            }
+            else if(mp.find(queries[i][0])==mp.end()&&un1.find(queries[i][1])!=un1.end())
+            {
+                mp[queries[i][0]]=queries[i][1];
+                un1[queries[i][1]]++;
+                cout<<"3 "<<queries[i][0]<<" "<<un1[mp[queries[i][0]]]<<" ";
+            }
+            else
+            {
+                if(mp[queries[i][0]]!=queries[i][1])
+                {
+                    
+                    un1[mp[queries[i][0]]]--;
+                    if(un1[mp[queries[i][0]]]==0)
+                    {
+                        c--;
+                        un1.erase(mp[queries[i][0]]);
+                    }
+                    mp[queries[i][0]]=queries[i][1];
+                    un1[mp[queries[i][0]]]++;
+                }
+                    cout<<"4 "<<queries[i][0]<<" "<<un1[mp[queries[i][0]]]<<" ";
+            }
+            ans.push_back(c);
+        }
+        return ans;
+    }
+};
