524aeba7d1c60f176493c07aaadacfc553cc8401
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2025-02-10 00:41:47 +0530

    Time: 47 ms (33.96%), Space: 49.4 MB (25.07%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -2,33 +2,31 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,1e9));
-       int ans = solve(coins,amount,n,dp);
-       return (ans==1e9)?-1:ans;
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        int ans = solve( coins, amount, n-1, dp);
+        return(ans==1e9)?-1:ans;
     }
-    int solve(vector<int>& coins, int amount,int ind,vector<vector<int>>&dp)
+    int solve(vector<int>& coins, int amount, int ind,vector<vector<int>>&dp)
     {
-        for(int i=0;i<=amount;i++)
+        if(amount==0)return 0;
+        if(ind==0)
         {
-            if(i%coins[0]==0)
+            if(amount%coins[0]==0)
             {
-                dp[0][i]=i/coins[0];
+                return amount/coins[0];
+            }
+            else
+            {
+                return 1e9;
             }
         }
-        for(int i=1;i<ind;i++)
+        if(dp[ind][amount]!=-1)return dp[ind][amount];
+        int nottake= solve(coins,amount,ind-1,dp);
+        int take =1e9;
+        if(coins[ind]<=amount)
         {
-            for(int j=0;j<=amount;j++)
-            {
-                int nottake = dp[i-1][j];
-                int take = 1e9;
-                if(j>=coins[i])
-                {
-                    take = 1+dp[i][j-coins[i]];
-                }
-                dp[i][j]=min(take,nottake);
-            }
+            take = 1+solve(coins,amount-coins[ind],ind,dp);
         }
-        return dp[ind-1][amount];
+        return dp[ind][amount] = min(take,nottake);
     }
-
 };
