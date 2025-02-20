68b12a6208e4486698149fafa63d4af9348ea2b3
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2025-02-20 17:17:00 +0530

    Time: 0 ms (100%), Space: 13.6 MB (27.53%) - LeetHub

---
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+unordered_set<string>numset;
+int n;
+    string generate(string curr)
+    {
+        if(curr.size()==n)
+        {
+            if(numset.find(curr)==numset.end())
+            {
+                return curr;
+            }
+            return "";
+        }
+        string addzero=generate(curr+"0");
+        if(addzero.size()>0)
+        {
+            return addzero;
+        }
+        return generate(curr+"1");
+    }
+    string findDifferentBinaryString(vector<string>& nums) {
+        n = nums.size();
+        for(string s:nums)
+        {
+            numset.insert(s);
+        }
+        return generate("");
+    }
+};
