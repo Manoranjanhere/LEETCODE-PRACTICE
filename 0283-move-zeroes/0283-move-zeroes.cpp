4e348020ec1dd627a1cba2a5646c31480659b0dc
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2025-02-24 00:42:41 +0530

    Time: 0 ms (100%), Space: 24 MB (18.14%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,17 +1,8 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        int l =0;
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]!=0)
-            {
-                swap(nums[i],nums[l]);
-                {
-                    l++;
-                }
-            }
-        }
+       stable_partition(nums.begin(),nums.end(),[](int n){
+        return n!=0;
+       });
     }
 };
