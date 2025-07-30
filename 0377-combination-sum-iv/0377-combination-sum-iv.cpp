f6121b772a02fd601e6dc8128441bcb2cf6a623f
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2025-07-30 16:13:34 +0530

    Time: 0 ms (100%), Space: 10.3 MB (7.87%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-    int combinationSum4(vector<int>& nums, int target) 
-    {
-        int c=0;
-        vector<int> dp(target+1,-1);
-        solve(nums,target,0,c,dp);
-        return c;
+    int combinationSum4(vector<int>& nums, int target) {
+	int n = nums.size();
+	vector<vector<int>>dp(n,vector<int>(target+1,-1));
+	return solve(nums,target,dp,0);
     }
-    void solve(vector<int>& nums,int target,int sum,int & c,vector<int>& dp)
-    {
-        if(sum==target)
-        {
-            c++;
-            return;
-        }
-        if(sum>target)
-        {
-            return;
-        }
-        if(dp[sum]!=-1)
-        {
-            c+=dp[sum];
-            return;
-        }
-        int inc = c;
-        for(int i=0;i<nums.size();i++)
-        {
-            solve(nums,target,sum+nums[i],c,dp);
-        }
-        dp[sum]=c-inc;
-    }
+	int solve(vector<int>& nums, int target,vector<vector<int>>&dp,int ind)
+	{
+		if(target==0)
+		{
+			return 1;
+		}
+		if(ind==nums.size()||target<=0)
+		{
+			return 0;
+		}
+		if(dp[ind][target]!=-1)
+		{
+			return dp[ind][target];
+		}
+		int take=solve(nums,target-nums[ind],dp,0);
+		int nottake=solve(nums,target,dp,ind+1);
+		return dp[ind][target]=take+nottake;
+	
+	}
+	
 };
