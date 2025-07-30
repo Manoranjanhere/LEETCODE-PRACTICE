19dcc039c2335a179a4293cc1a4a91c1d6882978
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2025-07-30 15:27:33 +0530

    Time: 31 ms (63.27%), Space: 19.1 MB (61.95%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,32 +1,24 @@
-
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-	vector<vector<int>>dp(coins.size(),vector<int>(amount+1,1e9));
-        for (int i = 0; i < coins.size(); i++) {
-            dp[i][0] = 0;
-        }
-          for (int j = coins[0]; j <= amount; j++) {
-            if (j % coins[0] == 0) {
-                dp[0][j] = j / coins[0];
+        vector<int> dp(amount + 1, -1);
+        int ans = solve(coins, amount, dp);
+        return ans == INT_MAX ? -1 : ans;
+    }
+
+    int solve(vector<int>& coins, int amount, vector<int>& dp) {
+        if (amount == 0) return 0;
+        if (amount < 0) return INT_MAX;
+        if (dp[amount] != -1) return dp[amount];
+
+        int mini = INT_MAX;
+        for (int coin : coins) {
+            int res = solve(coins, amount - coin, dp);
+            if (res != INT_MAX) {
+                mini = min(mini, res + 1);
             }
         }
-       for(int i=1;i<coins.size();i++)
-       {
-        for(int j=1;j<=amount;j++)
-        {
-            if(j>=coins[i])
-            {
-                dp[i][j]=min(dp[i-1][j],1+dp[i][j-coins[i]]);
-            }
-            else
-            {
-                dp[i][j]=dp[i-1][j];
-            }
-        }
-       }
-        int ans =dp[coins.size()-1][amount];
-        if(ans==1e9)return -1;
-        return ans;
-	}
+
+        return dp[amount] = mini;
+    }
 };
