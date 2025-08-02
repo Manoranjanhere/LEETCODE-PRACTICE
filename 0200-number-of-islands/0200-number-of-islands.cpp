2a5b385d1b76b6afadc66b9e941d9682f7d79eef
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2025-08-02 13:18:15 +0530

    Time: 25 ms (72.61%), Space: 17.4 MB (51.74%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,57 +1,31 @@
 class Solution {
 public:
-    void bfs(vector<vector<char>>& grid,int i,int j,vector<vector<bool>>&vis,int m,int n)
-    {
-        queue<pair<int,int>> q;
-        q.push({i,j});
-        while(!q.empty())
-        {
-            int sz=q.size();
-            for(int k=0;k<sz;k++)
-            {
-                pair<int,int> curr = q.front();
-                q.pop();
-                int x = curr.first;
-                int y= curr.second;
-                if(x+1<m&&grid[x+1][y]=='1'&&vis[x+1][y]==false)
-                {
-                    vis[x+1][y]=true;
-                    q.push({x+1,y});
-                }
-                if(y+1<n&&grid[x][y+1]=='1'&&vis[x][y+1]==false)
-                {
-                    vis[x][y+1]=true;
-                    q.push({x,y+1});
-                }
-                if(y>0&&grid[x][y-1]=='1'&&vis[x][y-1]==false)
-                {
-                    vis[x][y-1]=true;
-                    q.push({x,y-1});
-                }
-                if(x>0&&grid[x-1][y]=='1'&&vis[x-1][y]==false)
-                {
-                    vis[x-1][y]=true;
-                    q.push({x-1,y});
-                }
-            }
-        }
-    }
     int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        int c=0;
-        vector<vector<bool>> vis(m,vector<bool>(n,false));
-        for(int i=0;i<m;i++)
+        int n = grid.size();
+        int m = grid[0].size();
+        int ans = 0;
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<m;j++)
             {
-                if(grid[i][j]=='1'&&vis[i][j]==false)
+                if(grid[i][j]=='1'&&!vis[i][j])
                 {
-                    c++;
-                    bfs(grid,i,j,vis,m,n);
+                    ans++;
+                    solve(grid,vis,i,j);
                 }
+
             }
         }
-        return c;
+        return ans;
+    }
+    void solve(vector<vector<char>>& grid,vector<vector<int>>&vis,int i,int j)
+    {
+        if(i<0||i>=grid.size()||j<0||j>=grid[0].size()||grid[i][j]=='0'||vis[i][j]==1)return;
+        vis[i][j]=1;
+        solve(grid,vis,i+1,j);
+        solve(grid,vis,i,j+1);
+        solve(grid,vis,i-1,j);
+        solve(grid,vis,i,j-1);
     }
 };
