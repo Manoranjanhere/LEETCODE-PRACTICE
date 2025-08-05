d9cbec91293fdb4e7e902043715a30726a4b5f71
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2025-08-06 01:34:00 +0530

    Time: 0 ms (100%), Space: 9 MB (88.17%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
+        unordered_map<char,string> mp;
+        mp['2']="abc";
+        mp['3']="def";
+        mp['4']="ghi";
+        mp['5']="jkl";
+        mp['6']="mno";
+        mp['7']="pqrs";
+        mp['8']="tuv";
+        mp['9']="wxyz";
         vector<string> ans;
-        if(digits.size()==0)
-        {
-            return ans;
-        }
-        map<int,string> mp;
-        mp[2]="abc";
-        mp[3]="def";
-        mp[4]="ghi";
-        mp[5]="jkl";
-        mp[6]="mno";
-        mp[7]="pqrs";
-        mp[8]="tuv";
-        mp[9]="wxyz";
-        ans.push_back("");
-        for(int i:digits)
+        if(digits.size()==0)return ans;
+        ans.push_back({""});
+        for(char i:digits)
         {
             vector<string> temp;
-            string a = mp[i-'0'];
-            for(auto j: ans)
+            for(auto ch1:ans)
             {
-                for(char ch:a)
-                {
-                    temp.push_back(j+ch);
+                for(char ch2:mp[i])
+                {   
+                    temp.push_back(ch1+ch2);
                 }
             }
             ans.swap(temp);
-
         }
         return ans;
     }
