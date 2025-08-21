2a55f5df384f580cdff130e81c6dcaf7f90ed985
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2025-08-21 16:53:22 +0530

    Time: 0 ms (100%), Space: 115.6 MB (78.16%) - LeetHub

--- 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
@@ -2,26 +2,37 @@ class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
 	int cand1=tops[0];
-	int cand2=bottoms[0];
-	int top=  solve(tops,bottoms,cand1);
-	int bottom=solve(tops,bottoms,cand2);
-	int ans = min(top,bottom);
-	return (ans==INT_MAX)?-1:ans;
-	
-      }
-	int solve(vector<int>& tops, vector<int>& bottoms,int x)
-	{
+	int cand2=bottoms[0];	
 		int top=0;
 		int bottom=0;
 		for(int i=0;i<tops.size();i++)
 		{
-		if(x!=tops[i]&&x!=bottoms[i])
+		if(cand1!=tops[i]&&cand1!=bottoms[i])
 		{
-			return INT_MAX;
+			top=INT_MAX;
+            bottom=INT_MAX;
+            break;
 		}
-		else if(x!=tops[i])top++;
-		else if(x!=bottoms[i])bottom++;
+		else if(cand1!=tops[i])top++;
+		else if(cand1!=bottoms[i])bottom++;
 		}
-	return min(top,bottom);
+	int ans1 = min(top,bottom);
+		top=0;
+		bottom=0;
+		for(int i=0;i<tops.size();i++)
+		{
+		if(cand2!=tops[i]&&cand2!=bottoms[i])
+		{
+			top=INT_MAX;
+            bottom=INT_MAX;
+            break;
+		}
+		else if(cand2!=tops[i])top++;
+		else if(cand2!=bottoms[i])bottom++;
+		}
+	int ans2 = min(top,bottom);
+	int ans = min(ans1,ans2);
+	return (ans==INT_MAX)?-1:ans;
+
 	}
 };
