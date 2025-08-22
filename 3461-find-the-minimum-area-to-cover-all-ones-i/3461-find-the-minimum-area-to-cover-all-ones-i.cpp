35ffc78e0f3c496c70033029e97a5e7f6eaeb89b
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2025-08-22 11:54:52 +0530

    Time: 271 ms (23.32%), Space: 133.4 MB (86.9%) - LeetHub

--- 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.cpp
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.cpp
@@ -1,26 +1,81 @@
 class Solution {
 public:
     int minimumArea(vector<vector<int>>& grid) {
+	int top =0;
+	int bottom=-1;
+	int left=0;
+	int right=0;
 	int n = grid.size();
 	int m = grid[0].size();
-	int top =n;
-	int bottom=-1;
-	int left=m;
-	int right=-1;
-        for(int i=0;i<n;i++)
+    bool got=false;
+    for(int i=0;i<n;i++)
 	{
 		for(int j=0;j<m;j++)
 		{
 			if(grid[i][j]==1)
 			{
-				top=min(top,i);
-	bottom= max(bottom,i);
-	left=min(left,j);
-	right=max(right,j);
+				top=i;
+                got=true;
+				break;
 			}
 		}
+        if(got)
+        {
+            break;
+        }
 	}
-	
+    got=false;
+	for(int i=n-1;i>=0;i--)
+	{
+		for(int j=0;j<m;j++)
+		{
+			if(grid[i][j]==1)
+			{
+                got=true;
+				bottom=i;
+				break;
+			}
+		}
+        if(got)
+        {
+            break;
+        }
+	}
+    got=false;
+	for(int j=0;j<m;j++)
+	{
+		for(int i=0;i<n;i++)
+		{
+			if(grid[i][j]==1)
+			{
+                got=true;
+				left=j;
+				break;
+			}
+		}
+        if(got)
+        {
+            break;
+        }
+	}
+    got=false;
+	for(int j=m-1;j>=0;j--)
+	{
+		for(int i=0;i<n;i++)
+		{
+			if(grid[i][j]==1)
+			{
+                got=true;
+				right=j;
+				break;
+			}
+		}
+        if(got)
+        {
+            break;
+        }
+	}
+    if(bottom==-1)return 0;
 	int ans = (right-left+1)*(bottom-top+1);
 	return ans;
     }
