db63a7dd3a3619264a877a755455ba6910bc94fa
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2025-08-23 23:35:44 +0530

    Time: 15 ms (16.21%), Space: 11 MB (40.21%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,24 @@
+
+     class Solution {
+public:
+    int characterReplacement(string s, int k) {
+	int n = s.size();
+	unordered_map<int,int> mp;
+	int c=0;
+	int ans =0;
+	int ind =0;
+	for(int i=0;i<n;i++)
+	{
+		mp[s[i]]++;
+		c=max(c,mp[s[i]]);
+		while(i-ind+1-c>k)
+		{
+			mp[s[ind]]--;
+			ind++;
+		}
+		ans=max(ans,i-ind+1);
+	}
+	
+	return ans;
+     }
+};
