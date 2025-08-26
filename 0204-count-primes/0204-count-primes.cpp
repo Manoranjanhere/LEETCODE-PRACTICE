21b5a9e0be24e79b7b8061cc66dca76a3a346f4d
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2025-08-26 21:16:02 +0530

    Time: 146 ms (89.87%), Space: 132.6 MB (17.76%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        vector<int>seive(n+1,1);
-        for(int i=2;i*i<n;i++)
-        {
-            if(seive[i]==0)continue;
-            int ind=i;
-            while(i*ind<n)
-            {
-                seive[i*ind]=0;
-                ind++;
+        if (n <= 2) return 0;  // no primes < 2
+        
+        vector<int> seive(n, 1);
+        seive[0] = seive[1] = 0;  // 0 and 1 are not prime
+
+        for (int i = 2; i * i < n; i++) {
+            if (seive[i] == 0) continue;
+            int ind = i * i;
+            while (ind < n) {
+                seive[ind] = 0;
+                ind += i;
             }
         }
-        int ans =0;
-        for(int i=2;i<n;i++)
-        {
-            if(seive[i]==1)ans++;
+
+        int ans = 0;
+        for (int i = 2; i < n; i++) {
+            if (seive[i] == 1) ans++;
         }
         return ans;
     }
