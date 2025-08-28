4cebfc494b5210d0389f0661a5e08bfdd5c7c4ae
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2025-08-28 13:02:07 +0530

    Time: 0 ms (100%), Space: 12.5 MB (32.62%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int left = 0;
-        int n = nums.size();
-        if(n<=1)return 0;
-        if(nums[0]>nums[1])return 0;
-        if(nums[n-1]>nums[n-2])return n-1;
-        int right = nums.size()-1;
-        while(left<right)
-        {
-            int mid = left+(right-left)/2;
-            if(mid!=0&&mid!=n-1&&nums[mid]>nums[mid+1]&&nums[mid]>nums[mid-1])
-            {
-                return mid;
-            }
-            if(nums[mid]>nums[mid+1])
-            {
-                right=mid;
-            }
-            else
-            {
-                left=mid+1;
-            }
-        }
-        return -1;
-    }
+		int n = nums.size();
+		if(n==1)return 0;
+		if(nums[0]>nums[1])return 0;
+		if(nums[n-1]>nums[n-2])return n-1;
+		int l=0;
+		int r = n-1;
+		while(l<=r)
+		{
+			int mid = l+(r-l)/2;
+			if(mid>0&&mid<n-1&&nums[mid]>nums[mid+1]&&nums[mid]>nums[mid-1])return mid;
+			if(nums[mid]<nums[mid+1])
+			{
+				l=mid+1;
+			}
+			else
+			{
+				r=mid;
+			}
+		}
+		return l;
+           }
 };
