4a91e90c053c3b7949587a17b80a4782ce539603
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2025-09-03 13:43:59 +0530

    Time: 8 ms (42.46%), Space: 10.7 MB (27.03%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int n1 = s1.length();
-        int n2 = s2.length();
-        if(n1>n2) return false;
-        vector<int> c1(26,0);
-        vector<int> c2(26,0);
-        for(char c:s1)
-        {
-            c1[c-'a']++;
-        }
-        for(int i=0;i<n1;i++)
-        {
-            c2[s2[i]-'a']++;
-        }
-        if(c1==c2) return true;
-        for(int i=n1;i<n2;i++)
-        {
-            c2[s2[i]-'a']++;
-            c2[s2[i-n1]-'a']--;
-            if(c2==c1) return true;
-        }
-        return false;
+        unordered_map<char,int> mp1;
+	unordered_map<char,int> mp2;
+	int n1 = s1.size();
+	int n2 = s2.size();
+    if(n2<n1)return false;
+	for(int i=0;i<n1;i++)
+	{
+		mp1[s1[i]]++;
+		mp2[s2[i]]++;
+	}
+	if(mp1==mp2)return true;
+	int ind =0;
+	for(int i=n1;i<n2;i++)
+	{
+		mp2[s2[ind]]--;
+		mp2[s2[i]]++;
+		if(mp2[s2[ind]]==0)mp2.erase(s2[ind]);
+		if(mp1==mp2)
+		{
+			return true;
+		}
+		ind++;
+	}
+	return false;
     }
 };
