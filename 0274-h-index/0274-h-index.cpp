49c4e223a74f601b216c75bfb455f21bd5ac2ee9
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2026-01-08 17:15:10 +0530

    Time: 0 ms (100%), Space: 13.6 MB (5.46%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
-    int hIndex(vector<int>& citations)
-    {
-        int n = citations.size();
-        if(n==1&&citations[0]==0)return 0;
-        if(n==1)return 1;
-        sort(citations.begin(),citations.end());
-         int c=0;
-         for(int i=citations[n-1];i>=0;i--)
-         {
-            c=0;
-            for(int j=n-1;j>=0;j--){
-                if(citations[j]>=i){
-                    c++;
-                }
-                if(c==i){
-                    return c;
-                }
-            }
-         }
-         return 0;
+    int hIndex(vector<int>& citations) {
+        sort(citations.rbegin(),citations.rend());
+	int c=0;
+	for(int i=0;i<citations.size();i++)
+	{
+		if(citations[i]>i)c++;
+		else break;
+	}
+	return c;
     }
 };
