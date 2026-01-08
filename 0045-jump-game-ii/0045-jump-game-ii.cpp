37611a98f1715805d0055486677c59149d9b3157
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2026-01-08 15:21:04 +0530

    Time: 0 ms (100%), Space: 21.5 MB (20.85%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -2,26 +2,22 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        if (n == 1) return 0;
-
-        int jumps = 0;     // number of jumps to reach the end
-        int farthest = 0;  // farthest point that can be reached
-        int current_end = 0; // end of the range that can be reached with the current number of jumps
-
-        for (int i = 0; i < n - 1; ++i) {
-            // update the farthest point that can be reached
-            farthest = max(farthest, i + nums[i]);
-
-            // if we've reached the end of the range for the current jump, update the jump count
-            if (i == current_end) {
-                jumps++;
-                current_end = farthest;
-
-                // if the farthest point is the end or beyond, break early
-                if (current_end >= n - 1) break;
-            }
-        }
-
-        return jumps;
+        if(n<=1)return 0;
+	vector<int> st(n);
+	st[0]=nums[0];
+	for(int i=1;i<n;i++)
+	{
+		st[i]=max(st[i-1]-1,nums[i]);
+	}
+	int step=1;
+	int k=nums[0];
+	int i=nums[0];
+	while(k<n-1)
+	{
+		k=i+st[i];
+        i=k;
+		step++;
+	}
+	return step;
     }
 };
