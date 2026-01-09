ef2e28a621d4b92a07386b3e51bf4cf1990fc985
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2026-01-09 15:17:03 +0530

    Time: 0 ms (100%), Space: 22.8 MB (70.51%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int n = ratings.size();
-        vector<int> arr(n,1);
-        for(int i=1;i<n;i++)
+        int n= ratings.size();
+	vector<int> st(n,1);
+	for(int i=1;i<n;i++)
+	{
+		if(ratings[i]>ratings[i-1]&&st[i]<=st[i-1]){int diff=st[i-1]+1-st[i];
+            st[i]+=diff;
+};
+	}
+	for(int i=n-2;i>=0;i--)
+	{
+		if(ratings[i]>ratings[i+1]&&st[i]<=st[i+1])
         {
-            if(ratings[i]>ratings[i-1])
-            {
-                arr[i]=arr[i-1]+1;
-            }
+	    int diff=st[i+1]+1-st[i];
+            st[i]+=diff;
         }
-        for(int i=n-2;i>=0;i--)
-        {
-            if(ratings[i]>ratings[i+1])
-            {
-                arr[i]=arr[i+1]+1;
-            }
-        }
-        int sum=0;
-        for(int i=0;i<n;i++)
-        {
-            sum+=arr[i];
-        }
-        return sum;
-        
+	}
+	int ans =0;
+	for(int i:st)
+	{
+		ans+=i;
+	}
+	return ans;
     }
 };
