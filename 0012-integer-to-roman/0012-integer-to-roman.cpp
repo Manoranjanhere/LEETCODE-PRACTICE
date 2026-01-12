df21bb55a3911efeae680ec99bc4ecd832d972b9
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2026-01-12 12:00:12 +0530

    Time: 0 ms (100%), Space: 8 MB (96.65%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,74 +1,60 @@
 class Solution {
 public:
-    string intToRoman(int num) {
+  string intToRoman(int sum) {        
 	string ans ="";
-        while(num>=1000)
+	while(sum>=1000)
 	{
 		ans+='M';
-		num-=1000;
+		sum-=1000;
 	}
-	if(num>=900)
-	{
-		ans+="CM";
-		num-=900;
-		
-	}
-	if(num>=500)
+	if(sum >= 900) { ans += "CM"; sum -= 900; }
+	if(sum>=500)
 	{
 		ans+='D';
-		num-=500;
+		sum-=500;
 	}
-	if(num>=400)
+	if(sum>=400)
 	{
 		ans+="CD";
-		num-=400;
+		sum-=400;
 	}
-	while(num>=100)
+	while(sum>=100)
 	{
 		ans+='C';
-		num-=100;
+		sum-=100;
 	}
-	if(num>=90)
-	{
-		ans+="XC";
-		num-=90;
-	}
-	if(num>=50)
+	if(sum >= 90) { ans += "XC"; sum -= 90; }
+	if(sum>=50)
 	{
 		ans+='L';
-		num-=50;
+		sum-=50;
 	}
-	if(num>=40)
+	if(sum>=40)
 	{
 		ans+="XL";
-		num-=40;
+		sum-=40;
 	}
-	while(num>=10)
+	while(sum>=10)
 	{
 		ans+='X';
-		num-=10;
+		sum-=10;
 	}
-	if(num>=9)
-	{
-		ans+="IX";
-		num-=9;
-	}
-	if(num>=5)
+	if(sum >= 9) { ans += "IX"; sum -= 9; }
+	if(sum>=5)
 	{
 		ans+='V';
-		num-=5;
+		sum-=5;
 	}
-	if(num>=4)
+	if(sum>=4)
 	{
 		ans+="IV";
-		num-=4;
+		sum-=4;
 	}
-	while(num>=1)
+	while(sum>=1)
 	{
 		ans+='I';
-		num-=1;
+		sum-=1;
 	}
 	return ans;
-	
     }
 };
