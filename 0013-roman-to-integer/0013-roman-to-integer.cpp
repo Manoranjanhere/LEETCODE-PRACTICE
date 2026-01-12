f9fbfad790ea1870d7b8101564f413001a75f959
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2026-01-12 11:31:09 +0530

    Time: 14 ms (12.36%), Space: 12.4 MB (62.77%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,32 +1,29 @@
-
-
 class Solution {
 public:
     int romanToInt(string s) {
-        int sum =0;
-	unordered_map<char,int> mp;
-	mp['I']=1;
-	mp['V']=5;
-	mp['X']=10;
-	mp['L']=50;
-	mp['C']=100;
-	mp['D']=500;
+        unordered_map<char,int> mp;
 	mp['M']=1000;
+	mp['D']=500;
+	mp['C']=100;
+	mp['L']=50;
+	mp['X']=10;
+	mp['V']=5;
+	mp['I']=1;
+	int sum = 0;
 	int n = s.size();
-	if(n==1)return mp[s[0]];
+    if(n==1)return mp[s[0]];
 	for(int i=0;i<n-1;i++)
 	{
-		if(mp[s[i]]<mp[s[i+1]])
+		if(mp[s[i]]>=mp[s[i+1]])
 		{
-			sum+=mp[s[i+1]]-mp[s[i]];
-            i++;
+			sum+=mp[s[i+1]];
+			if(i==0)sum+=mp[s[i]];
 		}
 		else
 		{
-			sum+=mp[s[i]];
+			sum+=mp[s[i+1]]-mp[s[i]];			if(i!=0)sum-=mp[s[i]];	
 		}
 	}
-	if(mp[s[n-2]]>=mp[s[n-1]])sum+=mp[s[n-1]];
 	return sum;
     }
-}; 
+};
