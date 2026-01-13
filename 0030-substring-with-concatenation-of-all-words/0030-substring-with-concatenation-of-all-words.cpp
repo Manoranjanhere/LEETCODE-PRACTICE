c2219924d0dd6ea728cfc30d857e92ca09395bd2
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2026-01-13 13:02:45 +0530

    Time: 47 ms (21.98%), Space: 36.4 MB (17.43%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -2,45 +2,36 @@ class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
         int n = words.size();
-        int one = words[0].size();
-        int total = n * one;
-        vector<int> ans;
+	int one = words[0].size();
+	int total= n*one;
+	vector<int> ans;
+	if(s.size()<total)return ans;
+	unordered_map<string,int> str;
+	for(string i:words)
+	{
+		str[i]++;
+	}
+	for (int start = 0; start < one; start++) {
+	unordered_map<string,int> mp;
+	for(int i=start;i+one<=start+total;i+=one)
+	{
+		string get=s.substr(i,one);
+		mp[get]++;
+	}
+	int j=start;
+	if(mp==str)ans.push_back(j);
+	while(j+total+one<=s.size())
+	{
+		string del = s.substr(j,one);
+		mp[del]--;
+		if(mp[del]==0) mp.erase(del);
+		string add= s.substr(j+total,one);
+		mp[add]++;
+		j+=one;
+		if(mp==str)ans.push_back(j);
 
-        if (s.size() < total) return ans;
-
-        unordered_map<string,int> str;
-        for (string w : words) str[w]++;
-
-        // try all offsets
-        for (int start = 0; start < one; start++) {
-
-            unordered_map<string,int> mp;
-            int j = start;
-
-            // build first window
-            for (int i = start; i + one <= start + total; i += one) {
-                string get = s.substr(i, one);
-                mp[get]++;
-            }
-
-            if (mp == str) ans.push_back(j);
-
-            // slide window
-            while (j + total + one <= s.size()) {
-
-                string del = s.substr(j, one);
-                mp[del]--;
-                if (mp[del] == 0) mp.erase(del);
-
-                j += one;
-
-                string add = s.substr(j + total - one, one);
-                mp[add]++;
-
-                if (mp == str) ans.push_back(j);
-            }
-        }
-
-        return ans;
+	}
+	}
+	return ans;
     }
 };
