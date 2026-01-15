37ce0036deb83869f87281d93ef423727fcf1c7a
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2026-01-15 16:02:47 +0530

    Time: 0 ms (100%), Space: 18.9 MB (5.19%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,39 +1,26 @@
-#include <vector>
-#include <algorithm> // For std::find
-
 class Solution {
 public:
-    void setZeroes(std::vector<std::vector<int>>& matrix) {
-        std::vector<int> rows;
-        std::vector<int> cols;
-        int n = matrix.size();
-        int m = matrix[0].size();
-        
-        // First pass: record all rows and columns that have zeros
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == 0) 
-                {
-                        rows.push_back(i);
-                    
-                        cols.push_back(j);
-                    
-                }
-            }
-        }
-        
-        // Second pass: set the rows to zero
-        for (int row : rows) {
-            for (int j = 0; j < m; j++) {
-                matrix[row][j] = 0;
-            }
-        }
-        
-        // Third pass: set the columns to zero
-        for (int col : cols) {
-            for (int i = 0; i < n; i++) {
-                matrix[i][col] = 0;
-            }
-        }
+    void setZeroes(vector<vector<int>>& matrix) {
+        unordered_set<int> ic;
+	unordered_set<int> jc;
+	for(int i=0;i<matrix.size();i++)
+	{
+		for(int j=0;j<matrix[0].size();j++)
+		{
+			if(matrix[i][j]==0)
+			{
+				ic.insert(i);
+				jc.insert(j);
+			}
+		}
+	}
+	for(int i=0;i<matrix.size();i++)
+	{
+		for(int j=0;j<matrix[0].size();j++)
+		{
+			if(ic.find(i)!=ic.end()||jc.find(j)!=jc.end())matrix[i][j]=0;
+		}
+	}
+
     }
 };
