9f5d517852def0f9be904004625c5a1f2a17c0dc
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2026-01-17 12:39:52 +0530

    Time: 1 ms (50.83%), Space: 19.2 MB (5.98%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        stack<int> st;
-	for(string i:tokens)
-	{
-		if(i=="+"||i=="-"||i=="*"||i=="/")
-		{
-			int d1 = st.top();
-			st.pop();
-			int d2 = st.top();
-			st.pop();
-			if(i=="+")st.push(d2+d1);
-			else if(i=="-")st.push(d2-d1);
-			else if(i=="*")st.push(d2*d1);
-			else if(i=="/")st.push(d2/d1);
-		}
-		else
-		{
-			st.push(stoi(i));
-		}
-	}
-	return st.top();
-    }	
+        stack<string> st;
+        for(string i:tokens)
+        {
+            if(i=="+"||i=="-"||i=="*"||i=="/")
+            {
+                int a = stoi(st.top());
+                st.pop();
+                int b = stoi(st.top());
+                st.pop();
+                if(i=="+")st.push(to_string(a+b));
+                if(i=="-")st.push(to_string(b-a));
+                if(i=="*")st.push(to_string(a*b));
+                if(i=="/")st.push(to_string(b/a));
+            }
+            else st.push(i);
+        }
+        return stoi(st.top());
+    }
 };
