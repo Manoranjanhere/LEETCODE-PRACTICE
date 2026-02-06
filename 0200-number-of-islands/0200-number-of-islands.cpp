97bfe35b76791bb37a9fb6697a68e7e10de1482a
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2026-02-06 12:23:16 +0530

    Time: 26 ms (63.38%), Space: 17.3 MB (51.85%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
+        int ans =0;
         int n = grid.size();
         int m = grid[0].size();
-        int ans = 0;
-        vector<vector<int>>vis(n,vector<int>(m,0));
+        vector<vector<int>> vis(n,vector<int>(m,0));
         for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(grid[i][j]=='1'&&!vis[i][j])
-                {
-                    ans++;
-                    solve(grid,vis,i,j);
-                }
-
-            }
-        }
+	    {
+		for(int j=0;j<m;j++)
+		{
+			if(!vis[i][j]&&grid[i][j]=='1')
+			{
+				ans++;
+				solve(vis,grid,i,j);
+			}
+		}
+	    }
         return ans;
     }
-    void solve(vector<vector<char>>& grid,vector<vector<int>>&vis,int i,int j)
+    void solve(vector<vector<int>>&vis,vector<vector<char>>&grid,int i,int j)
     {
-        if(i<0||i>=grid.size()||j<0||j>=grid[0].size()||grid[i][j]=='0'||vis[i][j]==1)return;
+        if(i<0||j<0||i>=grid.size()||j>=grid[0].size()||grid[i][j]=='0'||vis[i][j])return;
         vis[i][j]=1;
-        solve(grid,vis,i+1,j);
-        solve(grid,vis,i,j+1);
-        solve(grid,vis,i-1,j);
-        solve(grid,vis,i,j-1);
+        solve(vis,grid,i-1,j);
+        solve(vis,grid,i+1,j);
+        solve(vis,grid,i,j-1);
+        solve(vis,grid,i,j+1);
+        
     }
 };
